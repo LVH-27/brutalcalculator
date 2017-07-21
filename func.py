aa9b2721b1eb769ff657046258f318b8afac6611
rincewind <leonard.volaric-horvat@fer.hr>
2017-07-21 13:00:33 +0200

    Adding main and auxiliary functions.

---
+++ func.py
@@ -0,0 +1,13 @@
+hello_string = "Hello and welcome to ICM summer of code"
+
+for i in range(len(hello_string[0:15])):
+	print(hello_string[i::] + hello_string[0:i])
+
+print()
+
+
+def funkcija(a=0, b=0):
+	print("pozvana je funkcija za zbrajanje: {}*{}".format(a, b))
+
+	return a * b
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import func
+
+# Ovo je jednostavan program za zbrajanje dva broja
+
+A = 7
+B = 6
+
+print(func.funkcija(A, B))
